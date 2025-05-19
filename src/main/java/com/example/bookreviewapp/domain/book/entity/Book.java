46a3cc9043f90fdc0dc3205ce44d3de2199fcaba
kimminu7 <rock9024@naver.com>
2025-05-19 09:54:55 +0900

    feat : Book, EnrollStatus(Enum), 임시 User 엔티티 추가

--- src/main/java/com/example/bookreviewapp/domain/book/entity/Book.java
+++ src/main/java/com/example/bookreviewapp/domain/book/entity/Book.java
@@ -1,6 +1,35 @@
 package com.example.bookreviewapp.domain.book.entity;
 
 import com.example.bookreviewapp.common.entity.BaseEntity;
+import com.example.bookreviewapp.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
 
+@Entity
+@Getter
+@Table(name= "book")
 public class Book extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private User user_id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EnrollStatus enrollStatus = EnrollStatus.REJECT;
+
+
 }
--- src/main/java/com/example/bookreviewapp/domain/user/entity/User.java
+++ src/main/java/com/example/bookreviewapp/domain/user/entity/User.java
@@ -1,6 +1,16 @@
 package com.example.bookreviewapp.domain.user.entity;
 
 import com.example.bookreviewapp.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
 
+@Entity
+@Getter
+@Table(name="user")
 public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
