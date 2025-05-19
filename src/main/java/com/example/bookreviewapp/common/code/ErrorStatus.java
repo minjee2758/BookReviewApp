d6cddcd75fb26d8382adfc7cd8a27bb3ea23432b
alpomjeong <jsw00415@gmail.com>
2025-05-19 19:02:06 +0900

    fix : 줄 겹침 충돌 해결

--- src/main/java/com/example/bookreviewapp/common/code/ErrorStatus.java
+++ src/main/java/com/example/bookreviewapp/common/code/ErrorStatus.java
@@ -15,6 +15,8 @@ public enum ErrorStatus implements BaseErrorCode {
 
 	//review 에러코드
 
+
+
 	//admin 에러코드
 	BOOK_ADD_REQUEST_ALREADY_ACCEPT(HttpStatus.BAD_REQUEST,"4001","이미 승인된 요청입니다."),
 	REVIEW_ALREADY_PINNED(HttpStatus.BAD_REQUEST,"4002","이미 고정된 리뷰입니다."),
