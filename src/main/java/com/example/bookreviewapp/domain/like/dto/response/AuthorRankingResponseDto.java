0e43b42a7d899f836d0e1a2dcfbab3b25bc68ae2
alpomjeong <jsw00415@gmail.com>
2025-05-24 23:01:23 +0900

    fix : jsonproperty 어노테이션 추가

--- src/main/java/com/example/bookreviewapp/domain/like/dto/response/AuthorRankingResponseDto.java
+++ src/main/java/com/example/bookreviewapp/domain/like/dto/response/AuthorRankingResponseDto.java
@@ -1,12 +1,25 @@
 package com.example.bookreviewapp.domain.like.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class AuthorRankingResponseDto {
     private final Integer rank;
     private final String name;
     private final int score;
+
+    @JsonCreator
+    public AuthorRankingResponseDto(
+        @JsonProperty("rank") Integer rank,
+        @JsonProperty("name") String name,
+        @JsonProperty("score") int score
+    ) {
+        this.rank  = rank;
+        this.name  = name;
+        this.score = score;
+    }
 }
--- src/main/java/com/example/bookreviewapp/domain/like/dto/response/LikeResponseDto.java
+++ src/main/java/com/example/bookreviewapp/domain/like/dto/response/LikeResponseDto.java
@@ -1,17 +1,30 @@
 package com.example.bookreviewapp.domain.like.dto.response;
 
 import com.example.bookreviewapp.domain.like.entity.Like;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalTime;
 
 @Getter
-@AllArgsConstructor
 public class LikeResponseDto {
     private final Long bookId;
     private final String title;
     private final LocalTime likedAt;
+
+    @JsonCreator
+    public LikeResponseDto(
+        @JsonProperty("bookId") Long bookId,
+        @JsonProperty("title")  String title,
+        @JsonProperty("likedAt") LocalTime likedAt
+    ) {
+        this.bookId = bookId;
+        this.title  = title;
+        this.likedAt = likedAt;
+    }
 
     public static LikeResponseDto fromLiketoDto(Like like) {
         return new LikeResponseDto(
